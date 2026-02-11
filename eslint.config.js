a8f3281f9fd926b85d6f889bdd9abadd39119177
AlexMan123456 <alex.man03888@gmail.com>
2026-02-11 22:50:35 +0000

    Use the multiple configs pattern as is now supported by ESLint v10

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,3 @@
 import alexPlugin from "./dist/index.js";
 
-export default [
-  ...alexPlugin.configs["internal/eslint-plugin"],
-  ...alexPlugin.configs["general/package-json"],
-];
+export default alexPlugin.configs["general/package-json"];
---
+++ tests/eslint.config.js
@@ -0,0 +1,6 @@
+import alexPlugin from "../dist/index.js";
+
+export default [
+  ...alexPlugin.configs["combined/tests"],
+  ...alexPlugin.configs["internal/eslint-plugin"],
+];
