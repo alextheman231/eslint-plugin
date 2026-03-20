a58de35218047cdcee86dbef9f4e422ea294687e
AlexMan123456 <alex.man03888@gmail.com>
2026-03-20 00:26:52 +0000

    Disable require-param in components
    After thinking about it a bit, I don't think it's too helpful as the only argument in React components is just the props, which are better JSDoc commented in the interface rather than at the param level.

--- src/configs/internal/components.ts
+++ src/configs/internal/components.ts
@@ -7,9 +7,7 @@ const internalComponents: Array<Linter.Config> = [
   {
     rules: {
       "jsdoc/check-param-names": "off",
-      "jsdoc/require-param": ["error", { checkDestructured: false }],
-      // Not really helpful in components, since most of the time, they will be returning a ReactNode. It's more helpful to
-      // document the prop types and purpose of the component instead.
+      "jsdoc/require-param": "off",
       "jsdoc/require-returns": "off",
     },
   },
