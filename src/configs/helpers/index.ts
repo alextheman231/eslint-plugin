93dd426f313b6416a614a1d3e8cca2eadda418b3
AlexMan123456 <alex.man03888@gmail.com>
2026-04-04 23:39:43 +0100

    Fix typing issue with eslint-plugin-react-hooks
    It needs an explicit type annotation for some reason.

--- src/configs/helpers/index.ts
+++ src/configs/helpers/index.ts
@@ -1,6 +1,3 @@
-export { default as prettierConfig } from "src/configs/external/prettierConfig";
-export { default as typeDocConfig } from "src/configs/external/typeDocConfig";
-export { default as vitestConfig } from "src/configs/external/vitestConfig";
 export { default as reactLanguageOptions } from "src/configs/helpers/reactLanguageOptions";
 export { default as typeScriptLanguageOptions } from "src/configs/helpers/typeScriptLanguageOptions";
 export { default as unusedVarsIgnorePatterns } from "src/configs/helpers/unusedVarsIgnorePatterns";
