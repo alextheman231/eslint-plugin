1d218cb99b861c8d5baaba286956690bdd6ccb8f
AlexMan123456 <alex.man03888@gmail.com>
2026-04-04 23:38:49 +0100

    Remove Vitest peer dependency and internal Vitest config
    Given the smaller scale of it, I think it'd be better for all projects to define their own Vitest config per project rather than use a centralised one.

--- src/configs/external/index.ts
+++ src/configs/external/index.ts
@@ -1,3 +1,2 @@
 export { default as prettierConfig } from "src/configs/external/prettierConfig";
 export { default as typeDocConfig } from "src/configs/external/typeDocConfig";
-export { default as vitestConfig } from "src/configs/external/vitestConfig";
