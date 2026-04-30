8e8929db828d20a3df87e0e8e4c9147bb2b5dfbb
AlexMan123456 <alex.man03888@gmail.com>
2026-04-30 23:51:05 +0100

    Export the restricted imports for use in the Lexicon configs package directly
    For Lexicon, I would like to have it so that it owns all its configs, rather than having to go through this plugin every time.

---
+++ src/configs/helpers/restrictedImports/index.ts
@@ -0,0 +1,4 @@
+export { default as generalRestrictedImports } from "src/configs/helpers/restrictedImports/generalRestrictedImports";
+export { default as personalRestrictedImports } from "src/configs/helpers/restrictedImports/personalRestrictedImports";
+export { default as reactRestrictedImports } from "src/configs/helpers/restrictedImports/reactRestrictedImports";
+export { default as testsRestrictedImports } from "src/configs/helpers/restrictedImports/testsRestrictedImports";
