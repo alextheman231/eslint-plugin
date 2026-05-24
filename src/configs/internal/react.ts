90571203be5f7323d7b3cc004a625722425ae78f
AlexMan123456 <alex.man03888@gmail.com>
2026-05-24 14:04:05 +0100

    Replace usage of eslint-plugin-react with @eslint-react/eslint-plugin
    eslint-plugin-react as of now does not support ESLint v10, and its last release was over a year ago. This replaces that dependency with @eslint-plugin/eslint-react, which seems to catch more errors than eslint-plugin-react did anyway and does support ESLint v10.
    
    It also seems to provide a nice way to create custom React linting rules via @eslint-react/kit, which could be interesting to look into for our plugin especially!

--- src/configs/internal/react.ts
+++ src/configs/internal/react.ts
@@ -1,6 +1,5 @@
 import type { Linter } from "eslint";
 
-import reactPlugin from "eslint-plugin-react";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 import reactHooks from "src/configs/helpers/eslint-plugin-react-hooks";
@@ -12,7 +11,6 @@ const internalReact: Array<Linter.Config> = [
     languageOptions: reactLanguageOptions,
     name: "@alextheman/internal/react",
     plugins: {
-      react: reactPlugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
@@ -21,7 +19,7 @@ const internalReact: Array<Linter.Config> = [
       "react-hooks/exhaustive-deps": "off",
       "react-hooks/refs": "off",
       "react-refresh/only-export-components": "off",
-      "react/jsx-boolean-value": "error",
+      // "@eslint-react/jsx-boolean-value": "error", // TODO: Use @eslint-react/kit
     },
   },
 ];
